94c21ed50211b748a0f5d6e41c2e393fc5e954cc
Bex-G <bexgiacinto@gmail.com>
2024-08-05 16:45:01 -0700

    Make current hour 'now,' shorten forcast to 10 days, round temp numbers and restructure description div.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const input = document.querySelector("input");
 const searchBtn = document.getElementById("searchBtn");
 const current = document.getElementById("current");
 const hourly = document.getElementById("hourly");
-const forcast = document.getElementById("forcast");
+const daily = document.getElementById("daily");
 
 searchBtn.addEventListener("click", () => {
   if (input.value != "") {
@@ -24,12 +24,12 @@ window.addEventListener("keydown", (e) => {
 
 function clearData() {
   // remove any children of static HTML elements
-  if (forcast.childElementCount > 0) {
-    let forcastChildren = Array.from(forcast.children);
-    forcastChildren.forEach((element) => {
+  if (current.childElementCount > 0) {
+    let currentChildren = Array.from(current.children);
+    currentChildren.forEach((element) => {
       element.remove();
-      let currentChildren = Array.from(current.children);
-      currentChildren.forEach((element) => {
+      let dailyChildren = Array.from(daily.children);
+      dailyChildren.forEach((element) => {
         element.remove();
         let hourlyChildren = Array.from(hourly.children);
         hourlyChildren.forEach((element) => {
@@ -56,8 +56,9 @@ async function search(url) {
     const weatherData = await response.json();
     getCurrent(weatherData);
     getIcon(weatherData);
-    getWind(weatherData);
-    getWet(weatherData);
+    getDescription(weatherData);
+    // getWind(weatherData);
+    // getWet(weatherData);
     getHours(weatherData);
     getTable(weatherData);
     console.log(weatherData);
@@ -91,30 +92,45 @@ function formatTime(time) {
 function getIcon(weatherData) {
   let div = document.createElement("div");
   div.id = "iconBlock";
-  let temp = document.createElement("p");
-  temp.textContent = weatherData.currentConditions.temp + "°";
+  let p = document.createElement("p");
+  let temp = Number(weatherData.currentConditions.temp);
+  let modTemp = Math.round(temp);
+  p.textContent = modTemp + "°";
   let img = document.createElement("img");
   let icon = weatherData.currentConditions.icon;
   img.setAttribute("src", "../src/icons/" + icon + ".svg");
   img.setAttribute("alt", icon + " weather graphic icon");
-  let p = document.createElement("p");
-  let description = weatherData.description;
-  p.textContent = description;
-  div.appendChild(temp);
+  div.appendChild(p);
   div.appendChild(img);
   current.appendChild(div);
-  current.appendChild(p);
 }
 
-function getWind(weatherData) {
+function getDescription(weatherData) {
   let div = document.createElement("div");
+  div.id = "description";
   let p = document.createElement("p");
+  let description = weatherData.description;
   let windspeed = weatherData.currentConditions.windspeed;
   let winddir = weatherData.currentConditions.winddir;
   getDirection(winddir);
-  p.textContent = "Winds " + direction + " at " + windspeed + " mph." + " ";
+  let wind = "Winds " + direction + " at " + windspeed + " mph.";
+  let precipprob = weatherData.currentConditions.precipprob;
+  let precipitation = precipprob + "% chance of precipitation.";
+  p.textContent = description + " " + wind + " " + precipitation;
   div.appendChild(p);
   current.appendChild(div);
+}
+
+function getWind(weatherData) {
+  // let div = document.createElement("div");
+  // let p = document.createElement("p");
+  let windspeed = weatherData.currentConditions.windspeed;
+  let winddir = weatherData.currentConditions.winddir;
+  getDirection(winddir);
+  return (wind = "Winds " + direction + " at " + windspeed + " mph." + " ");
+  // p.textContent = "Winds " + direction + " at " + windspeed + " mph." + " ";
+  // div.appendChild(p);
+  // current.appendChild(div);
 }
 
 function getDirection(num) {
@@ -141,12 +157,12 @@ function getDirection(num) {
 }
 
 function getWet(weatherData) {
-  let div = document.createElement("div");
-  let p = document.createElement("p");
+  // let div = document.createElement("div");
+  // let p = document.createElement("p");
   let precipprob = weatherData.currentConditions.precipprob;
   p.textContent = precipprob + "% chance of precipitation.";
-  div.appendChild(p);
-  current.appendChild(div);
+  // div.appendChild(p);
+  // current.appendChild(div);
 }
 
 function getHours(weatherData) {
@@ -172,11 +188,15 @@ function displayHours(hours24) {
   for (let i = 0; i < hours24.length; i++) {
     let card = document.createElement("div");
     card.className = "card";
-
     let p1 = document.createElement("p");
-    let time = hours24[i].datetime.slice(0, 2);
-    formatHours(time);
-    p1.textContent = modHour;
+
+    if (i == 0) {
+      p1.textContent = "Now";
+    } else {
+      let time = hours24[i].datetime.slice(0, 2);
+      formatHours(time);
+      p1.textContent = modHour;
+    }
 
     let img = document.createElement("img");
     let icon = hours24[i].icon;
@@ -184,7 +204,9 @@ function displayHours(hours24) {
     img.setAttribute("alt", icon + " weather graphic icon");
 
     let p2 = document.createElement("p");
-    p2.textContent = hours24[i].temp + "°";
+    let temp = Number(hours24[i].temp);
+    let modTemp = Math.round(temp);
+    p2.textContent = modTemp + "°";
 
     card.appendChild(p1);
     card.appendChild(img);
@@ -203,22 +225,25 @@ function formatHours(time) {
 function getTable(weatherData) {
   let table = document.createElement("table");
   let th1 = document.createElement("th");
-  th1.textContent = "2-week forcast:";
+  th1.textContent = "10-day forcast:";
   let th2 = document.createElement("th");
-  th2.textContent = "temp-low";
+  th2.textContent = "";
   let th3 = document.createElement("th");
-  th3.textContent = "temp-high";
+  th3.textContent = "temp-low";
+  let th4 = document.createElement("th");
+  th4.textContent = "temp-high";
   table.appendChild(th1);
   table.appendChild(th2);
   table.appendChild(th3);
+  table.appendChild(th4);
 
   let days = weatherData.days;
 
-  for (let i = 0; i < days.length; i++) {
+  for (let i = 0; i < 10; i++) {
     let row = table.insertRow();
     let cell1 = row.insertCell();
 
-    if (days[i].datetime === days[0].datetime) {
+    if (i == 0) {
       cell1.textContent = "Today";
     } else {
       let date = days[i].datetime.replace(/-/g, ",");
@@ -227,11 +252,21 @@ function getTable(weatherData) {
     }
 
     let cell2 = row.insertCell();
-    cell2.textContent = days[i].tempmin + "°";
+    let img = document.createElement("img");
+    let icon = days[i].icon;
+    img.setAttribute("src", "../src/icons/" + icon + ".svg");
+    cell2.appendChild(img);
+
     let cell3 = row.insertCell();
-    cell3.textContent = days[i].tempmax + "°";
+    let min = Number(days[i].tempmin);
+    let modMin = Math.round(min);
+    cell3.textContent = modMin + "°";
+    let cell4 = row.insertCell();
+    let max = Number(days[i].tempmax);
+    let modMax = Math.round(max);
+    cell4.textContent = modMax + "°";
   }
-  forcast.appendChild(table);
+  daily.appendChild(table);
 }
 
 function getDayOfWeek(date) {
